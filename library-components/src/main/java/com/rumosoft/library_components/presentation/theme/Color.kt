6ef4d5ec022be3d5631800a8754698545cd42df9
Jep Rubio <jeprubio@gmail.com>
2022-06-02 21:42:01 +0200

    [Add] splash screen and colours

--- library-components/src/main/java/com/rumosoft/library_components/presentation/theme/Color.kt
+++ library-components/src/main/java/com/rumosoft/library_components/presentation/theme/Color.kt
@@ -2,7 +2,17 @@ package com.rumosoft.library_components.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val CadetBlue = Color(0xFFAAB8C2)
+val CatskillWhite = Color(0xFFF5F8FA)
+val DodgerBlue = Color(0xFF1DA1F2)
+val Mystic = Color(0xFFE1E8ED)
+val PaleSky = Color(0xFF657786)
+val White = Color(0xFFFFFFFF)
+val Woodsmoke = Color(0xFF14171A)
+
+val Blue = DodgerBlue
+val Black = Woodsmoke
+val DarkGray = PaleSky
+val LightGray = CadetBlue
+val ExtraLightGray = Mystic
+val ExtraExtraLightGray = CatskillWhite
--- library-components/src/main/java/com/rumosoft/library_components/presentation/theme/Theme.kt
+++ library-components/src/main/java/com/rumosoft/library_components/presentation/theme/Theme.kt
@@ -7,24 +7,23 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = White,
+    primaryVariant = DarkGray,
+    secondary = Blue,
+    surface = White,
+    onPrimary = Black,
+    onSecondary = Black,
+    onSurface = Black,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Black,
+    primaryVariant = DarkGray,
+    secondary = Blue,
+    surface = Black,
+    onPrimary = White,
+    onSecondary = Black,
+    onSurface = White,
 )
 
 @Composable
